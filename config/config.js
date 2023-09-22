894121c419844ca073c3acf07847deef6ce5d03c
OEURN SEYHA <seyhaoeurn920@gmail.com>
2023-09-23 02:10:47 +0700

    update

--- config/config.js
+++ config/config.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-mongoose.connect(process.env.MONGOURL, {useUnifiedTopology: true, useNewUrlParser: true})
-.then(() => {
-    console.log('connect successfully');
+mongoose.connect(process.env.MONGOURL)
+.then((result) => {
+    console.log("connect successfully")
 }).catch((err) => {
-    console.log(err);
+    res.json({message: err});
+
 });
--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const cors = require('cors');
-
 const app = express();
 
 require('dotenv').config();
