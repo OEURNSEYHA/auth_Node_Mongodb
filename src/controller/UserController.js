800719f3f170681a5c65bbdaa007d31a7ff5726c
OEURN SEYHA <seyhaoeurn920@gmail.com>
2023-09-23 10:10:33 +0700

    update

--- src/controller/UserController.js
+++ src/controller/UserController.js
@@ -1,34 +1,27 @@
-
-const { default: isEmail } = require('validator/lib/isemail');
-const Users = require('../model/Users');
+const { isEmail, isStrongPassword } = require("validator");
+const Users = require("../model/Users");
+const bcrypt = require("bcrypt");
 
 const UserController = {
+  get: async (req, res) => {
+    res.json({ message: "Hello" });
+  },
 
-    get: async(req, res)=>{
-        res.json({message: "Hello"})
-    },
+  register: async (req, res) => {
+    try {
+      const { name, email, password } = req.body;
+      if (!name || !email || !password) throw Error("Please Input Failed");
+      if (!isEmail(email)) throw Error("Email Invalid");
+      if (!isStrongPassword(password)) throw Error("Password Not Strong");
+      const salt = await bcrypt.genSalt();
+      const hashPassword = await bcrypt.hash(password, salt)
+      const user = await Users.create({ name, email, password: hashPassword });
 
-    register: async(req, res)=>{
-        try{
-            const {name, email, password } = req.body;
-            if(!name || !email|| !password){
-                throw Error("Please Input Failed")
-            }
-            if(!isEmail(email)){
-                throw Error("Email Invalid")
-            }
-            if(!isStrongPassword(password)){
-                throw Error("Password Not Strong")
-            }
-            const user = await Users.create({name,email, password});
-            res.json(user);
-        }catch(err){
-            res.json({error: err.message});
-        
-        }
-      
+      res.json(user);
+    } catch (err) {
+      res.json({ error: err.message });
     }
-
-}
+  },
+};
 
 module.exports = UserController;
