ff5d08a1558e607901e8f47b5e38c4050287502f
OEURN SEYHA <seyhaoeurn920@gmail.com>
2023-10-06 22:00:45 +0700

    update

--- src/index.js
+++ src/index.js
@@ -1,19 +1,44 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const app = express();
-require('dotenv').config();
-require('../config/config')
+const swaggerJsDoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
+require("dotenv").config();
+require("../config/config");
 app.use(express.json());
 app.use(cors());
-const cookieParser = require('cookie-parser')
-const userRoute = require('./router/userRouter');
+const cookieParser = require("cookie-parser");
+const userRoute = require("./router/userRouter");
 
-app.use(userRoute)
-app.use(cookieParser())
-app.listen(process.env.PORT, (err)=>{
-    if(err){
-        console.log(err)
-    }else{
-        console.log('http://localhost:'+ process.env.PORT)
-    }
-})
+app.use(userRoute);
+app.use(cookieParser());
+
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Node Js API Project for mongodb",
+      version: "1.0.0",
+    },
+    servers: [
+      {
+        url: "http://localhost:8080/",
+        
+      },
+    ],
+   
+  },
+  apis: ["../src/router/userRouter.js"],
+  module: ['./model/Users.js']
+};
+
+const swaggerSpec = swaggerJsDoc(options);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+app.listen(process.env.PORT, (err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("http://localhost:" + process.env.PORT);
+  }
+});
